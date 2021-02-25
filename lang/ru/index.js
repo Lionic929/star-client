478d522f6660a45ded7878cc4b1dce9f427f074e
Lionic :-) <79619640+Lionic929@users.noreply.github.com>
2021-02-25 08:07:05 +0300

    Add files via upload
    Client files (APP) 2

---
+++ lang/ru/index.js
@@ -0,0 +1,27 @@
+import meta from './meta.json';
+import validation from './validation.json';
+import error from './error.json';
+import navigation from './navigation.json';
+import staticData from './static.json';
+import login from './login.json';
+import logout from './logout.json';
+import signup from './signup.json';
+import recovery from './recovery.json';
+import settings from './settings.json';
+import common from './common.json';
+
+export default {
+  translation: {
+    META: meta,
+    VALIDATION: validation,
+    ERROR: error,
+    NAVIGATION: navigation,
+    STATIC: staticData,
+    LOGIN: login,
+    LOGOUT: logout,
+    SIGNUP: signup,
+    RECOVERY: recovery,
+    SETTINGS: settings,
+    COMMON: common,
+  },
+};
